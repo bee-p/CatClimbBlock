7e9e0072507013eea7e8d17c7f46081d6ae1603d
bee-p <weileenw@naver.com>
2023-06-09 23:18:11 +0900

    [Add] 발판, 점수 UI 이미지 추가 및 배치
    - 발판
      >> 빨강, 파랑, 초록 발판 이미지 추가 후 prefab으로 만듬
    
    - 점수 UI
      >> 점수 UI 배경 이미지 추가, 점수 텍스트 길이에 따라 UI 이미지 늘어나게끔 적용, UI 텍스트 폰트 적용
      >> 텍스트 폰트는 Neo둥근모Pro(Neo둥근고딕Pro) 사용

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rigid2D;
+
     void Start()
     {
-        
+        this.rigid2D = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+
     }
 }
